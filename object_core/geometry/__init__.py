3dd8a349fef91403f20421965cefcf6104f6a790
Jason Hodges <41268044+jhodges0845@users.noreply.github.com>
2026-09-07 15:05:30 -0500

    Expose topology validation helpers

--- object_core/geometry/__init__.py
+++ object_core/geometry/__init__.py
@@ -3,5 +3,13 @@
 
 from .deformable import generate_deformable_mesh
 from .generator import generate_mesh
+from .topology import boundary_edges, edge_use_counts, is_closed_manifold, nonmanifold_edges
 
-__all__ = ["generate_mesh", "generate_deformable_mesh"]
+__all__ = [
+    "generate_mesh",
+    "generate_deformable_mesh",
+    "edge_use_counts",
+    "boundary_edges",
+    "nonmanifold_edges",
+    "is_closed_manifold",
+]
