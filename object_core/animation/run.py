b8a3bd7dc7b22e2813b76aa450f1033e95a3b228
Jason Hodges <41268044+jhodges0845@users.noreply.github.com>
2026-09-10 22:34:54 -0500

    Make human run use a distinct bent-limb gait

--- object_core/animation/run.py
+++ object_core/animation/run.py
@@ -28,8 +28,20 @@ def _keys(duration, degrees, strength):
     )
 
 
+def _pose_keys(duration, degrees, strength):
+    """Sample an asymmetric closed joint arc for joints that stay flexed while running."""
+    quarter = duration / 4
+    return tuple(
+        (time, radians(angle) * strength)
+        for time, angle in zip(
+            (0.0, quarter, 2 * quarter, 3 * quarter, duration),
+            degrees,
+        )
+    )
+
+
 def generate_run(duration=0.72, strength=1.0):
-    """Return a closed in-place run cycle with stronger opposing limb drive."""
+    """Return a closed in-place run cycle with a visibly different running silhouette."""
     for name, value, lower, upper in (("duration", duration, 0.35, 2.0), ("strength", strength, 0.1, 2.0)):
         if isinstance(value, bool) or not isinstance(value, (float, int)):
             raise TypeError(name + " must be a number")
@@ -37,14 +49,18 @@ def generate_run(duration=0.72, strength=1.0):
             raise ValueError(name + " must be between " + str(lower) + " and " + str(upper))
 
     tracks = (
-        RotationTrack("upper_leg.left", (1.0, 0.0, 0.0), _keys(duration, 38.0, strength)),
-        RotationTrack("upper_leg.right", (1.0, 0.0, 0.0), _keys(duration, -38.0, strength)),
-        RotationTrack("lower_leg.left", (1.0, 0.0, 0.0), _keys(duration, -28.0, strength)),
-        RotationTrack("lower_leg.right", (1.0, 0.0, 0.0), _keys(duration, 28.0, strength)),
-        RotationTrack("upper_arm.left", (1.0, 0.0, 0.0), _keys(duration, -34.0, strength)),
-        RotationTrack("upper_arm.right", (1.0, 0.0, 0.0), _keys(duration, 34.0, strength)),
-        RotationTrack("forearm.left", (1.0, 0.0, 0.0), _keys(duration, -14.0, strength)),
-        RotationTrack("forearm.right", (1.0, 0.0, 0.0), _keys(duration, 14.0, strength)),
-        RotationTrack("torso", (0.0, 0.0, 1.0), _keys(duration, 5.0, strength)),
+        RotationTrack("upper_leg.left", (1.0, 0.0, 0.0), _keys(duration, 48.0, strength)),
+        RotationTrack("upper_leg.right", (1.0, 0.0, 0.0), _keys(duration, -48.0, strength)),
+        RotationTrack("lower_leg.left", (1.0, 0.0, 0.0),
+                      _pose_keys(duration, (-28.0, -72.0, -42.0, -88.0, -28.0), strength)),
+        RotationTrack("lower_leg.right", (1.0, 0.0, 0.0),
+                      _pose_keys(duration, (42.0, 88.0, 28.0, 72.0, 42.0), strength)),
+        RotationTrack("upper_arm.left", (1.0, 0.0, 0.0), _keys(duration, -42.0, strength)),
+        RotationTrack("upper_arm.right", (1.0, 0.0, 0.0), _keys(duration, 42.0, strength)),
+        RotationTrack("forearm.left", (1.0, 0.0, 0.0),
+                      _pose_keys(duration, (-62.0, -92.0, -70.0, -102.0, -62.0), strength)),
+        RotationTrack("forearm.right", (1.0, 0.0, 0.0),
+                      _pose_keys(duration, (70.0, 102.0, 62.0, 92.0, 70.0), strength)),
+        RotationTrack("torso", (0.0, 0.0, 1.0), _keys(duration, 7.0, strength)),
     )
     return RunClip(float(duration), tracks)
