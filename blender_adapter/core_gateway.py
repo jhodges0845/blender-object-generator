4353f7833b357a551ce6b84e8b055e938b830a4b
Jason Hodges <41268044+jhodges0845@users.noreply.github.com>
2026-09-11 07:14:21 -0500

    Expose canonical provider-key migration helper

--- blender_adapter/core_gateway.py
+++ blender_adapter/core_gateway.py
@@ -12,6 +12,7 @@ AssetSnapshot = import_module(_core_name + '.models.validation').AssetSnapshot
 validate_asset = import_module(_core_name + '.validation').validate_asset
 _objects = import_module(_core_name + '.objects')
 OBJECT_TYPES = _objects.OBJECT_TYPES
+canonical_provider_key = _objects.canonical_provider_key
 get_provider = _objects.get_provider
 
 ValidationIssue = import_module(_core_name + '.models.validation').ValidationIssue
