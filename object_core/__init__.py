f7858980bfe2b625e2795f998e04bb32fc1dd671
Jason Hodges <41268044+jhodges0845@users.noreply.github.com>
2026-09-12 08:58:59 -0500

    Export rig binding contract

--- object_core/__init__.py
+++ object_core/__init__.py
@@ -6,10 +6,19 @@ from .proportions import generate_proportions
 from .geometry import generate_mesh
 from .rigging import generate_skeleton
 from .objects import OBJECT_TYPES, get_provider
-from .components import AttachmentMode, ComponentKind, ComponentRecord, PhysicsIntent, component_document, validate_component
+from .components import (
+    AttachmentMode,
+    ComponentKind,
+    ComponentRecord,
+    PhysicsIntent,
+    RigBinding,
+    component_document,
+    component_from_document,
+    validate_component,
+)
 
 __all__ = ["BodyType", "HumanoidSpec", "HumanoidProportions", "ObjectMesh",
            "MeshPart", "Bone", "Skeleton", "generate_proportions", "generate_mesh",
            "generate_skeleton", "OBJECT_TYPES", "get_provider", "AttachmentMode",
-           "ComponentKind", "ComponentRecord", "PhysicsIntent", "component_document",
-           "validate_component"]
+           "ComponentKind", "ComponentRecord", "PhysicsIntent", "RigBinding",
+           "component_document", "component_from_document", "validate_component"]
